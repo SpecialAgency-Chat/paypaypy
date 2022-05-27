b7a3687529824ffb143d02800278c7018a51d2d4
KohnoseLami <64224091+KohnoseLami@users.noreply.github.com>
2022-05-27 18:02:03 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,22 @@
+from setuptools import setup
+with open('requirements.txt') as requirements:
+    required = requirements.read().splitlines()
 
+setup(
+    name='PayPayPy',
+    version='1.0.0',
+    description='PayPayのログインから情報の取得、リンクの受け取りや送金を行えます',
+    url='https://github.com/SpecialAgency-Chat/paypaypy',
+    author='神瀬来未',
+    author_email='info@vxxx.cf',
+    license='GPL-3.0',
+    keywords='paypay backend frontend api unofficial private',
+    packages=[
+        "PayPayPy",
+    ],
+    install_requires=required,
+    classifiers=[
+        'License :: OSI Approved :: GPL-3.0 License',
+        'Programming Language :: Python :: 3.9.10',
+    ],
+)
