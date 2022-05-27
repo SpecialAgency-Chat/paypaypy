e939aa25ae8cbc2253bd6b318dc4f92be61ec340
KohnoseLami <64224091+KohnoseLami@users.noreply.github.com>
2022-05-27 17:56:38 +0900

    Update login.py

--- examples/login.py
+++ examples/login.py
@@ -1,1 +1,12 @@
+from PayPayPy import PayPay
 
+paypay = PayPay()
+login_result = paypay.login("PHONENUMBER", "PASSWORD")
+if login_result.header.resultCode == "S0000":
+    print("ログイン成功！")
+    print("貴方のアクセストークン: " + login_result.payload.accessToken)
+elif login_result.header.resultCode == "S1004":
+    otp = input("OTP: ")
+    otp_result = paypay.login_otp(otp, login_result.payload)
+    print("ログイン成功！")
+    print("貴方のアクセストークン: " + otp_result.payload.accessToken)
